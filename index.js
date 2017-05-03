24857d0ba9749565fb1360d35326607e0945b096
Gonzalo Andreani <gonzaandreani@gmail.com>
2017-05-02 23:45:24 -0400

    Connect to test DB and load fake user data

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 var nacl = require("tweetnacl");
 
 var keyPair = nacl.sign.keyPair();
@@ -8,4 +9,20 @@ var signature = nacl.sign.detached(Buffer.from(message), keyPair.secretKey);
 
 console.log('Message:', message);
 console.log('Signature:', signature.length, 'bytes,', Buffer.from(signature).toString('base64').length, ' base64 chars');
+
+var knex = require('knex')({
+  client: process.env.KNEX_DB_CLIENT,
+  connection: {
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database : process.env.DB
+  }
+});
+
+console.log(knex.select().from('users').toString());
+
+knex.select().from('users').then(function(data) {
+  console.log(data);
+});
 
