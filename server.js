714f3d93c2dc1197b9c5c422518b68c2cf60a23a
Gonzalo Andreani <gonzaandreani@gmail.com>
2017-05-03 23:56:06 -0400

    Use staticify instead of asset rack

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 var express = require('express');
-var exphbs = require('express-handlebars');
+var path = require('path');
+var Handlebars = require('express-handlebars');
+var staticify = require("staticify")(path.join(__dirname, "static"));
 
 var app = express();
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+
+app.use(staticify.middleware);
+
+var hbs = Handlebars.create({
+  defaultLayout: 'main',
+  helpers: {
+    getAsset: staticify.getVersionedPath
+  }
+});
+
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
+    console.log(staticify.getVersionedPath('/styles.css'));
     res.render('home');
 });
 
 app.listen(3000, function() {
   console.log('Listening on port 3000');
 });
+
