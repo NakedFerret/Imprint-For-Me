6ab329f36eee7314cb343283e2ec9d70e71f82e2
Gonzalo Andreani <gonzaandreani@gmail.com>
2017-09-04 14:26:42 -0700

    Added utility to help curl endpoint

---
+++ create-prophecy.js
@@ -0,0 +1,23 @@
+var nacl = require('tweetnacl');
+var Database = require('better-sqlite3');
+var db = new Database('test.db');
+
+var keyPair = nacl.sign.keyPair();
+
+var randomNumber = Math.floor(Math.random() * 10000);
+
+db.prepare('INSERT INTO users (name, publicKey) VALUES (?,?)').run(
+'user' + randomNumber, Buffer.from(keyPair.publicKey));
+
+var timestamp = Math.floor(Date.now() / 1000);
+var message = process.argv[2];
+
+var signature = nacl.sign.detached(Buffer.from(message + timestamp), keyPair.secretKey);
+
+console.log(JSON.stringify({
+  message: message,
+  timestamp: timestamp + "",
+  publicKey: Buffer.from(keyPair.publicKey).toString('base64'),
+  signature: Buffer.from(signature).toString('base64')
+}));
+
