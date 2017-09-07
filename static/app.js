1b4befba0e5cb341f94309eeab2fb761ac5062a8
Gonzalo Andreani <gonzaandreani@gmail.com>
2017-09-06 19:39:17 -0700

    Update homepage message

--- static/app.js
+++ static/app.js
@@ -7,6 +7,25 @@ function decodeUTF8(s) {
   return b;
 };
 
+function encodeBase64(arr) {
+  var i, s = [], len = arr.length;
+  for (i = 0; i < len; i++) s.push(String.fromCharCode(arr[i]));
+  return btoa(s.join(''));
+};
+
+function validateBase64(s) {
+  if (!(/^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/.test(s))) {
+    throw new TypeError('invalid encoding');
+  }
+}
+
+function decodeBase64(s) {
+  validateBase64(s);
+  var i, d = atob(s), b = new Uint8Array(d.length);
+  for (i = 0; i < d.length; i++) b[i] = d.charCodeAt(i);
+  return b;
+};
+
 function onCreateClick() {
   console.log('Create message clicked');
   var messageInput = document.querySelector('#messageInput');
@@ -22,7 +41,7 @@ function onCreateClick() {
   console.log({
     message: message,
     timestamp: timestamp,
-    keyPair: keyPair,
-    signature
+    pubKey: encodeBase64(keyPair.publicKey),
+    signature: encodeBase64(signature)
   });
 }
